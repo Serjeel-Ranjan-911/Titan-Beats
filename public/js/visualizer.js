4702d0171cb422d422c4c21f2ff4a9ea599d0e1f
SerjeelRanjan <serjeel.ranjan@gmail.com>
2021-03-21 22:43:34 +0530

    bit smooth animation for circles

--- public/js/visualizer.js
+++ public/js/visualizer.js
@@ -8,14 +8,25 @@ window.onload = function () {
     const sun1 = document.querySelector(".audioCircle1");
     const sun2 = document.querySelector(".audioCircle2");
     const sun3 = document.querySelector(".audioCircle3");
+    var callBack;
+    // audio.onended = () => {
+    //     console.log("Ended");
+    //     sun1.style.transform = `scale(1)`;
+    //     sun2.style.transform = `scale(1)`;
+    //     sun3.style.transform = `scale(1)`;
+    //     window.cancelAnimationFrame(callBack);
+    //     src = null;
+    // };
 
     file.onchange = function () {
         var files = this.files;
         audio.src = URL.createObjectURL(files[0]);
         audio.load();
         audio.play();
+
         var context = new AudioContext();
         var src = context.createMediaElementSource(audio);
+
         var analyser = context.createAnalyser();
 
         src.connect(analyser);
@@ -29,29 +40,16 @@ window.onload = function () {
         var dataArray = new Uint8Array(bufferLength);
 
         function renderFrame() {
-            requestAnimationFrame(renderFrame);
+            callBack = requestAnimationFrame(renderFrame);
 
             analyser.getByteFrequencyData(dataArray);
 
-            let circle1 = dataArray[3].map(0, 255, 1, 2).toFixed(2) * 1.3;
-            let circle2 = dataArray[4].map(0, 255, 1, 2).toFixed(2) * 1.1;
-            let circle3 = dataArray[5].map(0, 255, 1, 2).toFixed(2);
+            let circle1 = dataArray[3].map(0, 255, 1, 1.6).toFixed(2) * 1.3;
+            let circle2 = dataArray[4].map(0, 255, 1, 1.6).toFixed(2) * 1.1;
+            let circle3 = dataArray[5].map(0, 255, 1, 1.6).toFixed(2);
             sun1.style.transform = `scale(${circle1})`;
             sun2.style.transform = `scale(${circle2})`;
             sun3.style.transform = `scale(${circle3})`;
-
-            // for (var i = 0; i < bufferLength; i++) {
-            //     barHeight = dataArray[i];
-
-            //     var r = barHeight + 25 * (i / bufferLength);
-            //     var g = 250 * (i / bufferLength);
-            //     var b = 50;
-
-            //     ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-            //     ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
-
-            //     x += barWidth + 1;
-            // }
         }
 
         audio.play();
