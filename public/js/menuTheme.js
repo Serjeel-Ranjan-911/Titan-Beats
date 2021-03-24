1206dd3ce93222cbbfe25ca221ae09d3a82063a1
SerjeelRanjan <serjeel.ranjan@gmail.com>
2021-03-24 18:04:11 +0530

    added theme switch for menupage

---
+++ public/js/menuTheme.js
@@ -0,0 +1,21 @@
+var theme = "dark";
+let root = document.documentElement;
+
+const switchTheme = () => {
+    if (theme === "light") {
+        theme = "dark";
+        root.style.setProperty("--page-color", "#ebecf1");
+        root.style.setProperty("--shadow-down", "inset 4.5px 4.5px 9px #cbcbcb, inset -4.5px -4.5px 9px #ffffff;");
+        root.style.setProperty("--shadow-up", "4.5px 4.5px 9px #cbcbcb, -4.5px -4.5px 9px #ffffff");
+    } else {
+        theme = "light";
+        root.style.setProperty("--page-color", "#191a1e");
+        root.style.setProperty(
+            "--shadow-down",
+            "inset 6px 6px 12px #0e0e0e, inset -4px -4px 10px rgba(95, 94, 94, 0.25)"
+        );
+        root.style.setProperty("--shadow-up", "6px 6px 12px #0e0e0e, -4px -4px 10px rgba(95, 94, 94, 0.25)");
+    }
+};
+
+document.querySelector(".theme").addEventListener("click", switchTheme);
