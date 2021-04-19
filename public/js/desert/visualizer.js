bb47d147a4c62ddfe39af1e86dce3e6b94be00eb
Serjeel-Ranjan-911 <serjeel.19cs@kct.ac.in>
2021-04-19 20:20:27 +0530

    no more beep

--- public/js/desert/visualizer.js
+++ public/js/desert/visualizer.js
@@ -1,71 +1,71 @@
-import { getPath } from "./player.js";
+import { getPath } from './player.js';
 
 //maths function to map value range
 Number.prototype.map = function (in_min, in_max, out_min, out_max) {
-    return ((this - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+	return ((this - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
 
 window.onload = function () {
-    var audio = document.getElementById("audio");
-    const sun1 = document.querySelector(".audioCircle1");
-    const sun2 = document.querySelector(".audioCircle2");
-    const sun3 = document.querySelector(".audioCircle3");
+	var audio = document.getElementById('audio');
+	const sun1 = document.querySelector('.audioCircle1');
+	const sun2 = document.querySelector('.audioCircle2');
+	const sun3 = document.querySelector('.audioCircle3');
 
-    audio.src = "/introTune/beep-28.mp3";
-    audio.play();
-    var callBack;
-    // audio.onended = () => {
-    //     console.log("Ended");
-    //     sun1.style.transform = `scale(1)`;
-    //     sun2.style.transform = `scale(1)`;
-    //     sun3.style.transform = `scale(1)`;
-    //     window.cancelAnimationFrame(callBack);
-    //     src = null;
-    // };
+	audio.src = '/introTune/silence.mp3';
+	audio.play();
+	var callBack;
+	// audio.onended = () => {
+	//     console.log("Ended");
+	//     sun1.style.transform = `scale(1)`;
+	//     sun2.style.transform = `scale(1)`;
+	//     sun3.style.transform = `scale(1)`;
+	//     window.cancelAnimationFrame(callBack);
+	//     src = null;
+	// };
 
-    const player = function () {
-        //var files = this.files;
-        //audio.src = URL.createObjectURL(files[0]);
-        //const mood = window.location.href.substring(window.location.href.lastIndexOf("/") + 1);
-        const mood = document.getElementById("getMood").value;
-        getPath(path => {
-            audio.src = `/music/${mood}/` + path;
-            audio.load();
-            audio.play();
-        }, mood);
-        //audio.src = "/music/Liquid_Logo_Intro_Music.mp3";
+	const player = function () {
+		//var files = this.files;
+		//audio.src = URL.createObjectURL(files[0]);
+		//const mood = window.location.href.substring(window.location.href.lastIndexOf("/") + 1);
+		const mood = document.getElementById('getMood').value;
+		getPath((path) => {
+			audio.src = `/music/${mood}/` + path;
+			audio.load();
+			audio.play();
+		}, mood);
+		//audio.src = "/music/Liquid_Logo_Intro_Music.mp3";
 
-        var context = new AudioContext();
-        var src = context.createMediaElementSource(audio);
+		var context = new AudioContext();
+		var src = context.createMediaElementSource(audio);
 
-        var analyser = context.createAnalyser();
+		var analyser = context.createAnalyser();
 
-        src.connect(analyser);
-        analyser.connect(context.destination);
+		src.connect(analyser);
+		analyser.connect(context.destination);
 
-        analyser.fftSize = 32;
+		analyser.fftSize = 32;
 
-        var bufferLength = analyser.frequencyBinCount;
-        console.log(bufferLength);
+		var bufferLength = analyser.frequencyBinCount;
+		console.log(bufferLength);
 
-        var dataArray = new Uint8Array(bufferLength);
+		var dataArray = new Uint8Array(bufferLength);
 
-        function renderFrame() {
-            callBack = requestAnimationFrame(renderFrame);
+		function renderFrame() {
+			callBack = requestAnimationFrame(renderFrame);
 
-            analyser.getByteFrequencyData(dataArray);
+			analyser.getByteFrequencyData(dataArray);
 
-            let circle1 = dataArray[3].map(0, 255, 1, 1.6).toFixed(2) * 1.35;
-            let circle2 = dataArray[4].map(0, 255, 1, 1.6).toFixed(2) * 1.2;
-            let circle3 = dataArray[5].map(0, 255, 1, 1.6).toFixed(2);
-            sun1.style.transform = `scale(${circle1})`;
-            sun2.style.transform = `scale(${circle2})`;
-            sun3.style.transform = `scale(${circle3})`;
-        }
+			let circle1 = dataArray[3].map(0, 255, 1, 1.6).toFixed(2) * 1.35;
+			let circle2 = dataArray[4].map(0, 255, 1, 1.6).toFixed(2) * 1.2;
+			let circle3 = dataArray[5].map(0, 255, 1, 1.6).toFixed(2);
+			sun1.style.transform = `scale(${circle1})`;
+			sun2.style.transform = `scale(${circle2})`;
+			sun3.style.transform = `scale(${circle3})`;
+		}
 
-        audio.play();
-        renderFrame();
-    };
+		audio.play();
+		renderFrame();
+	};
 
-    audio.addEventListener("ended", player);
+	audio.addEventListener('ended', player);
 };
